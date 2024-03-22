f9e101c600e8e8df8944fb8b2bdf039db5d15672
DANIELJIMENEZH <159660355+DANIELJIMENEZH@users.noreply.github.com>
2024-03-22 15:30:08 -0600

    Add files via upload

---
+++ Empleado.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Resultado;
+
+/**
+ *
+ * @author Fer
+ */
+public class Empleado{
+  String nombre;
+
+    public Empleado(String nombre){
+        this.nombre = nombre;
+    }
+}
+
+class Directivo {
+    String nombre;
+
+    public Directivo(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
+class EmpleadoOperario {
+    String nombre;
+
+    public EmpleadoOperario(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
+class OperarioOficial {
+    String nombre;
+
+    public OperarioOficial(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
+class OperarioTecnico {
+    String nombre;
+    public OperarioTecnico(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
---
+++ Resultado.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.Resultado;
+/**
+ *
+ * @author FER
+ */
+public class Resultado {
+
+    public static void main(String[] args) {
+        
+         Empleado empleado = new Empleado("Rafa");
+        Directivo directivo = new Directivo("Mario");
+        EmpleadoOperario empleadoOperario = new EmpleadoOperario("Alfonso");
+        OperarioOficial operarioOficial = new OperarioOficial("Luis");
+        OperarioTecnico operarioTecnico = new OperarioTecnico("Pablo");
+        
+    }
+}
